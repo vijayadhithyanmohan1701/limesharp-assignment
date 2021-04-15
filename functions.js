3fce198633f5d3f04d636d2b976b77ed3c45ea1c
Gitium <gitium@presslabs.com>
2021-04-15 14:56:08 +1200

    Functions updated for testing

--- functions.js
+++ functions.js
@@ -1,8 +1,14 @@
+function repeat(arrVar){
+    let repeaterArr = Array(3);
+    repeaterArr.fill([...arrVar]);
+    console.log([].concat.apply([], [...repeaterArr]));
+}
+
 function reformat(str){
     let myStr = "";
     let words = str.split(" ");
     words.forEach(function(word, index){
-        console.log(word + " --> " + index);
+        //console.log(word + " --> " + index);
         let vowels = ['a', 'e', 'i', 'o', 'u'];
         word = [...word].map(letter => {
             if(vowels.includes(letter.toLowerCase())){
@@ -23,4 +29,26 @@ function reformat(str){
     });
     console.log(myStr);
 }
-reformat("hello there vIJaY aNd aNusHa");
+
+function next_binary_number(binaryArr) {
+    let decimal = 0;
+    let decStep = 0;
+    
+    for(var itr = binaryArr.length - 1 ; itr >= 0 ; itr--){
+        decimal = decimal + binaryArr[itr] * Math.pow(2, decStep);
+        //console.log(itr + " => " + binaryArr[itr] + " => " + decimal);
+        decStep++;
+    }
+    
+    let nextDecimal = decimal + 1;
+    let binary = 0;
+    let rem, i = 1, step = 1;
+    
+    while (nextDecimal != 0) {
+        rem = nextDecimal % 2;
+        nextDecimal = parseInt(nextDecimal / 2);
+        binary = binary + rem * i;
+        i = i * 10;
+    }
+    console.log(`Binary: ${[...String(binary)].map(Number)}`);
+}
